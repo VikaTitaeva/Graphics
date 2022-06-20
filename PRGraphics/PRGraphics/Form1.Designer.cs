b266290718f122f92c77e8dab10325f2aacb1818
VikaTitaeva <99070716+VikaTitaeva@users.noreply.github.com>
2022-06-20 20:47:12 +0700

    на

---
+++ PRGraphics/PRGraphics/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace PRGraphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.ok_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 95);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Purple;
+            series1.Legend = "Legend1";
+            series1.LegendText = "график";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(684, 324);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "x^2 cos(x)\t",
+            "sin^2(х^3)",
+            "sin (х^2) +х",
+            "(x^4) +(x^3) -x",
+            "ln(sinx+1)"});
+            this.comboBox1.Location = new System.Drawing.Point(715, 179);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(231, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(715, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(715, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(715, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(231, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // ok_btn
+            // 
+            this.ok_btn.Location = new System.Drawing.Point(715, 389);
+            this.ok_btn.Name = "ok_btn";
+            this.ok_btn.Size = new System.Drawing.Size(162, 30);
+            this.ok_btn.TabIndex = 5;
+            this.ok_btn.Text = "Построить";
+            this.ok_btn.UseVisualStyleBackColor = true;
+            this.ok_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(953, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Нижний интервал";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(953, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Верхний интервал";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(953, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Шаг";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 497);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ok_btn);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "м";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button ok_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PRGraphics/PRGraphics/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PRGraphics
+{
+    public partial class Form1 : Form
+    {
+        private double a, b, h;
+        private double x, y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void DefaultParams()
+        {
+            a = -10;
+            b = 10;
+            h = 0.1;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            h = Convert.ToDouble(textBox3.Text);
+            if (comboBox1.SelectedIndex==0)
+            {
+                x = a;
+                this.chart1.Series[0].Points.Clear();
+                while (x <= b)
+                {
+                    y = Math.Pow(x,2)*Math.Cos(x);
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                    x += h;
+                }
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                x = a;
+                this.chart1.Series[0].Points.Clear();
+                while (x <= b)
+                {
+                    y = Math.Pow(Math.Sin(Math.Pow(x,3)),2);
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                    x += h;
+                }
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                x = a;
+                this.chart1.Series[0].Points.Clear();
+                while (x <= b)
+                {
+                    y = Math.Sin(Math.Pow(x,2));
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                    x += h;
+                }
+            }
+            if (comboBox1.SelectedIndex == 3)
+            {
+                x = a;
+                this.chart1.Series[0].Points.Clear();
+                while (x <= b)
+                {
+                    y = Math.Pow(x,4)+Math.Pow(x,3)-x;
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                    x += h;
+                }
+            }
+            if (comboBox1.SelectedIndex == 4)
+            {
+                x = a;
+                this.chart1.Series[0].Points.Clear();
+                while (x <= b)
+                {
+                    y = Math.Log(Math.Sin(x + 1));
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                    x += h;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+       
+        
+
+        private void Postr_btn_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        public void grafics_cmbx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
